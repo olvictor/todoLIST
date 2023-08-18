00834e3a64e4e5ee826dc4ab5a35e7e818d601c2
VictorOlv <victorolvdev@gmail.com>
2023-08-18 17:35:03 -0300

    create form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { motion  } from 'framer-motion'
 import './App.css'
-
+import Form from './components/form'
 function App() {
   const [todos, setTodos] = useState([])
 
   return (
     <>
-    <div></div>
+    <Form />
+    <motion.div
+      style={{
+        width:400,
+        height:'auto',
+        borderRadius:10,
+        backgroundColor: "rgba(255,255,255,0.5)",
+        position:'relative',
+        padding: '20px'
+      }}
+      animate={{scale: [1.25,2,1.5,1]}}
+    >
+      <motion.li>aa</motion.li>
+      <motion.li>bb</motion.li>
+      <motion.li>cc</motion.li>
+      <motion.li>dd</motion.li>
+      <motion.li>ee</motion.li>
+      <motion.li>aa</motion.li>
+
+    </motion.div>
     </>
   )
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <form>
+        <input type="text" />
+        <button>Submit</button>
+    </form>
+  )
+}
+
+export default Form
